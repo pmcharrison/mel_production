338ff9c79f79e68e906d10311ceb014acbe620fa
syntheso <39628712+syntheso@users.noreply.github.com>
2019-10-10 20:56:14 +0100

    Create psychTestR_MelProd.R

---
+++ psychTestR_MelProd.R
@@ -0,0 +1,30 @@
+
+# setup
+dir = "/Users/sebsilas/Desktop/data-programming/coursework"
+setwd(dir)
+require(psychTestR)
+
+
+# create a page type for playing back midi
+
+
+midi_page <- function(body, admin_ui = NULL,
+         on_complete = NULL) {
+  ui <- shiny::tags$head(tags$script(src="https://eartrainer.app/melodic-production/js/midi.js"))
+  psychTestR::page(ui = ui, admin_ui = admin_ui, on_complete = on_complete)
+}
+
+
+# define a page to play midi
+mid_1 <- midi_page(actionButton("Play Melody", onclick="MIDIjs.play(\'https://www.eartrainer.app/melodic-production/stimuli/Berkowitz1.mid\');")) 
+  
+
+# create the timeline
+timeline <- list(
+  mid_1,
+  final_page("The end")
+  )
+
+# run the test
+test <- make_test(elts = timeline)
+shiny::runApp(test)
